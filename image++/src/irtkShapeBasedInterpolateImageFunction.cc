36aa8fe775aa76c8747459c95defa4b8881545bb
Monius <m1105930979@gmail.com>
2020-08-29 04:33:51 +0100

    fix error

--- image++/src/irtkShapeBasedInterpolateImageFunction.cc
+++ image++/src/irtkShapeBasedInterpolateImageFunction.cc
@@ -16,11 +16,8 @@
 
 #include <irtkResampling.h>
 
-#ifdef HAS_CONTRIB
-
 #include <irtkEuclideanDistanceTransform.h>
 
-#endif
 
 irtkShapeBasedInterpolateImageFunction::irtkShapeBasedInterpolateImageFunction()
 {
@@ -35,7 +32,7 @@ const char *irtkShapeBasedInterpolateImageFunction::NameOfClass()
   return "irtkShapeBasedInterpolateImageFunction";
 }
 
-#ifdef HAS_CONTRIB
+
 
 void irtkShapeBasedInterpolateImageFunction::Initialize()
 {
@@ -521,20 +518,20 @@ void irtkShapeBasedInterpolateImageFunction::Refine()
   }
 }
 
-double irtkShapeBasedInterpolateImageFunction::EvaluateInside(double x, double y, double z, double t)
-{
-  int i, j, k, l;
+// double irtkShapeBasedInterpolateImageFunction::EvaluateInside(double x, double y, double z, double t)
+// {
+//   int i, j, k, l;
 
-  _input->ImageToWorld(x,y,z);
-  _rinput.WorldToImage(x,y,z);
+//   _input->ImageToWorld(x,y,z);
+//   _rinput.WorldToImage(x,y,z);
 
-  i = round(x);
-  j = round(y);
-  k = round(z);
-  l = round(t);
+//   i = round(x);
+//   j = round(y);
+//   k = round(z);
+//   l = round(t);
 
-  return this->_rinput.GetAsDouble(i, j, k, l);
-}
+//   return this->_rinput.GetAsDouble(i, j, k, l);
+// }
 
 double irtkShapeBasedInterpolateImageFunction::Evaluate(double x, double y, double z, double t)
 {
@@ -669,26 +666,24 @@ double irtkShapeBasedInterpolateImageFunction::EvaluateLinear(double x, double y
   return val;
 }
 
-#else
+// void irtkShapeBasedInterpolateImageFunction::Initialize()
+// {
+//   /// Initialize baseclass
+//   this->irtkImageFunction::Initialize();
 
-void irtkShapeBasedInterpolateImageFunction::Initialize()
-{
-  /// Initialize baseclass
-  this->irtkImageFunction::Initialize();
+//   // Compute image domain
+//   this->_x = this->_input->GetX();
+//   this->_y = this->_input->GetY();
+//   this->_z = this->_input->GetZ();
 
-  // Compute image domain
-  this->_x = this->_input->GetX();
-  this->_y = this->_input->GetY();
-  this->_z = this->_input->GetZ();
-
-  // Compute domain on which the linear interpolation is defined
-  this->_x1 = -0.5;
-  this->_y1 = -0.5;
-  this->_z1 = -0.5;
-  this->_x2 = this->_input->GetX()-0.5;
-  this->_y2 = this->_input->GetY()-0.5;
-  this->_z2 = this->_input->GetZ()-0.5;
-}
+//   // Compute domain on which the linear interpolation is defined
+//   this->_x1 = -0.5;
+//   this->_y1 = -0.5;
+//   this->_z1 = -0.5;
+//   this->_x2 = this->_input->GetX()-0.5;
+//   this->_y2 = this->_input->GetY()-0.5;
+//   this->_z2 = this->_input->GetZ()-0.5;
+// }
 
 double irtkShapeBasedInterpolateImageFunction::EvaluateInside(double x, double y, double z, double t)
 {
@@ -702,21 +697,20 @@ double irtkShapeBasedInterpolateImageFunction::EvaluateInside(double x, double y
   return this->_input->GetAsDouble(i, j, k, l);
 }
 
-double irtkShapeBasedInterpolateImageFunction::Evaluate(double x, double y, double z, double t)
-{
-  int i, j, k, l;
+// double irtkShapeBasedInterpolateImageFunction::Evaluate(double x, double y, double z, double t)
+// {
+//   int i, j, k, l;
 
-  i = round(x);
-  j = round(y);
-  k = round(z);
-  l = round(t);
+//   i = round(x);
+//   j = round(y);
+//   k = round(z);
+//   l = round(t);
 
-  if ((i < 0) || (i >= this->_x) || (j < 0) || (j >= this->_y) || (k < 0) || (k >= this->_z)) {
-    return this->_DefaultValue;
-  } else {
-    return this->_input->GetAsDouble(i, j, k, l);
-  }
-}
+//   if ((i < 0) || (i >= this->_x) || (j < 0) || (j >= this->_y) || (k < 0) || (k >= this->_z)) {
+//     return this->_DefaultValue;
+//   } else {
+//     return this->_input->GetAsDouble(i, j, k, l);
+//   }
+// }
 
-#endif
 
